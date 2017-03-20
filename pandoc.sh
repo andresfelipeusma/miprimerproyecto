73aedec37c684c22588daba1da964367bb98a672
Andres Felipe <andresfelipeusma@gmail.com>
2017-03-20 09:49:03 +0100

    adeu adeu

--- pandoc.sh
+++ pandoc.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-pandoc --output=README.xhtml \
+pandoc --output=/var/tmp/pandocfiles/README.xhtml \
 	--standalone \
 	README.md
 
-xmllint --noout --valid README.xhtml 
+xmllint --noout --valid /var/tmp/pandocfiles/README.xhtml 
 
 # vim:ts=4:sw=4:et:ai
